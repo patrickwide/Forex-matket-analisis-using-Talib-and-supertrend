63a2cd4c4c125175c7a0ab22c89644dea00d3295
patrickWide <patpal911@gmail.com>
2022-03-24 11:43:03 +0300

    last commit

---
+++ functions.py
@@ -0,0 +1,17 @@
+import time
+
+
+#import talib as ta
+
+#print(ta)
+
+
+def run():
+ 
+   return "running"
+
+
+a = run()
+print(a)
+
+time.sleep(60)
---
+++ home.py
@@ -0,0 +1,33 @@
+import ccxt
+import config
+import schedule
+# import panda_learn as pd
+import pandas as pd
+
+
+
+data = pd.read_csv('data.csv')
+
+df = pd.DataFrame(data)
+print(data.tail(3))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
